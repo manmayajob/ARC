59eac63808d264a81f8cb23f5f68e234e55bf2af
manmayajob <manmaya7750@gmail.com>
2021-11-28 20:03:35 +0000

    final commit fornmanual_solve.py

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,24 +1,78 @@
 #!/usr/bin/python
 
+# Student Information
+# Student Name: Manmaya Prasad Panda 
+# Student ID: 17232977
+# Git: https://github.com/manmayajob/ARC
+
 import os, sys
 import json
 import numpy as np
 import re
+import matplotlib.pyplot as plt
 
 ### YOUR CODE HERE: write at least three functions which solve
 ### specific tasks by transforming the input x and returning the
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
-    return x
 
-def solve_b2862040(x):
-    return x
 
-def solve_05269061(x):
-    return x
 
+
+def solve_6150a2bd(inputs):
+    '''
+    In this function, numpy package to reverse the order of elements in an array, 
+    the elements are reordered but the shape is preserved.
+    
+    Parameters: c = ARC test list
+    
+    Returns: tst_rslt = returns a numpy ndarray, size of which depends on the task
+
+    '''
+
+    input_array = np.array(inputs)
+    tst_rslt = np.flip(input_array)
+    return tst_rslt
+
+def solve_ce22a75a(inputs):
+    '''
+    The function returns a numpy array by replacing all zeros around a pattern with the corresponding pattern value.
+    
+    '''
+    tst_rslt = np.array(inputs)
+    res = np.where(tst_rslt > 0)
+    try:
+        
+        for i in list(zip(res[0], res[1])):     
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if (x != 0 or y != 0):
+                        tst_rslt[i[0] - x][i[1] - y] = 1
+                        tst_rslt[i[0]][i[1]] = 1 
+    except IndexError: # catch the error
+        pass #we are passing the if any point is at the corner                         
+    return tst_rslt
+
+def solve_1cf80156(inputs):
+    '''
+
+    The function returns an numpy array by slicing the min and max value of rows and columns for the given input.
+    
+    '''
+
+    input_array = np.array(inputs)
+    result = np.where(input_array > 0)
+    column_minium = min(result[1])
+    column_maximum = max(result[1])
+    row_minimum = min(result[0])
+    row_maximum = max(result[0])
+    tst_rslt = []
+    for i in range(row_minimum,row_maximum + 1):
+        for j in range(column_minium,column_maximum + 1):
+            tst_rslt.append(input_array[i][j])
+    tst_rslt = np.reshape(tst_rslt,(row_maximum-row_minimum + 1,column_maximum-column_minium + 1))
+    return tst_rslt
 
 def main():
     # Find all the functions defined in this file whose names are
@@ -76,7 +130,8 @@ def test(taskID, solve, data):
     for x, y in zip(test_input, test_output):
         yhat = solve(x)
         show_result(x, y, yhat)
-
+    plt_list = train_input + test_input 
+    visualization_func(plt_list,taskID)  # added to show the grid visualisation of the results 
         
 def show_result(x, y, yhat):
     print("Input")
@@ -90,6 +145,19 @@ def show_result(x, y, yhat):
         print(f"False. Incorrect shape: {y.shape} v {yhat.shape}")
     else:
         print(np.all(y == yhat))
+
+def visualization_func(input,titlename):
+    '''
+    plotting grids and emulating testing interface.
+    
+    Parameters: input = A list of test input and computed output.
+
+    '''
+    for i in range(len(input)):
+        plt.matshow(input[i])
+
+    plt.title(titlename)    
+    plt.show()
 
 
 if __name__ == "__main__": main()
